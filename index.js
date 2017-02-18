e52392909e7677998e2e7f8253e6087385fc0683
richard-cargill <catdoce@gmail.com>
2017-02-18 09:51:52 +0000

    removing eslint and fixing standardjs errors.

--- index.js
+++ index.js
@@ -15,99 +15,100 @@ const TEMPLATE_LANG = 'pug'
 
 // getPaths( `${CONTENT_DIR}**/*.md` )
 // returns array of paths
-const getPaths = pattern => glob.sync( pattern )
+const getPaths = pattern => glob.sync(pattern)
 
 // If a file exists read content from path
 // returns string of content
-const readContentFromPath = path => fs.readFileSync( path, 'utf8' );
+const readContentFromPath = path => fs.readFileSync(path, 'utf8')
 
 // If content exists parse frontmatter
 // returns object of attributes
-const parseFrontmatter = content => fm( content )
+const parseFrontmatter = content => fm(content)
 
 // If content exists parse markdown
 // returns string of html
-const parseMarkdown = content => md( content )
+const parseMarkdown = content => md(content)
 
 // If date is valid, parses date by pattern
 // returns string of date
-const parseUnixtime = ( date, pattern ) => moment.unix( date ).format( pattern )
+const parseUnixtime = (date, pattern) => moment.unix(date).format(pattern)
 
 // Merge content from objects passed into single object
 // returns object with all objects values
-const mergeObjects = function(){
-  return Array.prototype.slice.call( arguments )
-    .reduce( ( accu, curr ) => Object.assign( accu, curr ), {})
+const mergeObjects = function () {
+  return Array.prototype.slice.call(arguments)
+    .reduce((accu, curr) => Object.assign(accu, curr), {})
 }
 
 // Write the contents to path
 // returns undefined
-const writeFile = ( path, contents ) => fs.writeFileSync( path, contents, 'utf-8' )
+const writeFile = (path, contents) => fs.writeFileSync(path, contents, 'utf-8')
 
 // Takes a path ending in .md and converts it to .html
 const updateMdExtHtml = dir => dir
-    .replace( path.extname( dir ), '.html' )
-    .replace( CONTENT_DIR, '' )
+    .replace(path.extname(dir), '.html')
+    .replace(CONTENT_DIR, '')
 
 // This needs refactoring
-const parseAll = ( contents, paths ) => {
+const parseAll = (contents, paths) => {
   let parseCounter = 0
 
-  return contents.reduce( ( accu, curr ) => {
-    let result = parseFrontmatter( curr )
-    result.body = parseMarkdown( result.body )
-    result.attributes.date = parseUnixtime( result.attributes.date, DATE_FORMAT )
+  return contents.reduce((accu, curr) => {
+    let result = parseFrontmatter(curr)
+    result.body = parseMarkdown(result.body)
+    result.attributes.date = parseUnixtime(result.attributes.date, DATE_FORMAT)
 
-    let pathName = updateMdExtHtml( paths[ parseCounter ] )
+    let pathName = updateMdExtHtml(paths[ parseCounter ])
 
     accu[ pathName ] = result
     parseCounter++
     return accu
-  }, {});
+  }, {})
 }
 
 // Ensure directory exists before creating files
 const ensureDirectoryExistence = filePath => {
-  var dirname = path.dirname( filePath )
-  if( fs.existsSync( dirname ) )
+  var dirname = path.dirname(filePath)
+  if (fs.existsSync(dirname)) {
     return true
-  ensureDirectoryExistence( dirname )
-  fs.mkdirSync( dirname )
+  }
+  ensureDirectoryExistence(dirname)
+  fs.mkdirSync(dirname)
 }
 
-const rmDirContents = globPattern => {
-  let all = glob.sync( globPattern ),
+// const rmDirContents = globPattern => {
+//   let all = glob.sync(globPattern),
+//
+//   // Separate files and directories
+//     files = all.filter(x => fs.statSync(x).isFile()),
+//     dir = all.filter(x => !fs.statSync(x).isFile())
+//
+//   // Sort array based on the length of string.
+//   // This ensures that nested folders come first
+//   dir.sort((a, b) => b.length - a.length)
+//
+//   // Delete
+//   files.map(x => fs.unlinkSync(x))
+//   dir.map(x => fs.rmdirSync(x))
+// }
 
-  // Separate files and directories
-  files = all.filter( x => fs.statSync( x ).isFile() ),
-  dir = all.filter( x => !fs.statSync( x ).isFile() )
-
-  // Sort array based on the length of string.
-  // This ensures that nested folders come first
-  dir.sort( ( a, b ) => b.length - a.length )
-
-  // Delete
-  files.map( x => fs.unlinkSync( x ) )
-  dir.map( x => fs.rmdirSync( x ) )
-}
-
-!function main(){
+!(function main () {
   // Get all paths of markdown files in content directory
-  let paths = getPaths( `${ CONTENT_DIR }**/*.md` )
+  let paths = getPaths(`${CONTENT_DIR}**/*.md`)
 
   // read content from paths and populate array
-  let contents = paths.map( x => readContentFromPath( x ) )
+  let contents = paths.map(x => readContentFromPath(x))
 
   // Parse all content
-  let parse = parseAll( contents, paths )
+  let parse = parseAll(contents, paths)
 
   // Merge parsed contents json with default json object
-  let defaultJson = JSON.parse( readContentFromPath( DEFAULT_CONFIG ) )
-  let mergeContentAndDefaults = mergeObjects( parse, defaultJson )
+  let defaultJson = JSON.parse(readContentFromPath(DEFAULT_CONFIG))
+  let mergeContentAndDefaults = mergeObjects(parse, defaultJson)
 
   // compile minified json file if p flat specified
-  let jsonOutput = argv.p ? JSON.stringify( mergeContentAndDefaults )
-    : JSON.stringify( mergeContentAndDefaults, null, 4 )
+  let jsonOutput = argv.p ? JSON.stringify(mergeContentAndDefaults)
+    : JSON.stringify(mergeContentAndDefaults, null, 4)
 
   // Add an enviroment flag to template files
   let env = argv.p ? 'prod' : 'dev'
@@ -116,22 +117,21 @@ const rmDirContents = globPattern => {
   // rmDirContents( `${ DIST_DIR }/**/**/*` )
 
   // Write out json in pretty format
-  writeFile( `${ DIST_DIR }data.json`, jsonOutput );
+  writeFile(`${DIST_DIR}data.json`, jsonOutput)
 
   // Loop parsed keys to generate html files
-  Object.keys( parse ).map( x => {
-    let pathname = `${ DIST_DIR }${ x }`
+  Object.keys(parse).map(x => {
+    let pathname = `${DIST_DIR}${x}`
     let el = parse[ x ]
     let compiledFunction = pug.compileFile(
-      `./src/${TEMPLATE_LANG}/${el.attributes.template}.${TEMPLATE_LANG}` )
+      `./src/${TEMPLATE_LANG}/${el.attributes.template}.${TEMPLATE_LANG}`)
 
-    let data = mergeObjects( el, {
+    let data = mergeObjects(el, {
       data: mergeContentAndDefaults,
       env: env
-    } )
+    })
 
-    ensureDirectoryExistence( pathname )
-    writeFile( pathname, compiledFunction( data ) )
+    ensureDirectoryExistence(pathname)
+    writeFile(pathname, compiledFunction(data))
   })
-
-}()
+}())
